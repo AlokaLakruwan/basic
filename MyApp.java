99e1be2342ad3a26293c6342316892ab9188c196
alpilk <aloka.piyasiri@ifs.com>
2025-09-05 15:36:53 +0530

    commit: colorized output

--- MyApp.java
+++ MyApp.java
@@ -1,5 +1,11 @@
 public class MyApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // ANSI escape codes for color
+        String ANSI_RESET = "\u001B[0m";
+        String ANSI_CYAN = "\u001B[36m";
+        String ANSI_PURPLE = "\u001B[35m";
+        System.out.println(ANSI_CYAN + "========================" + ANSI_RESET);
+        System.out.println(ANSI_PURPLE + "      Hello, World!      " + ANSI_RESET);
+        System.out.println(ANSI_CYAN + "========================" + ANSI_RESET);
     }
 }
